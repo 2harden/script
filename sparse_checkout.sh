8a2b3ca8f97448b371ffd11ac16fac6b12b974de
2harden <zc_shenchn@outlook.com>
2022-11-12 20:15:23 +0800

    upload scripts

---
+++ sparse_checkout.sh
@@ -0,0 +1,20 @@
+#REM Author: 2harden
+#REM Created Time: 2022/11/12
+#REM Release: 1.0
+#REM Description: sparse checkout
+
+#! /usr/bin/bash
+git_url=https://github.com/2harden/NiukeProblem.git
+
+shopt -s extglob  # 开启通配符功能
+rm -rf !(sparse_checkout.sh) # 删除目录下所有除了 sparse_checkout.sh 文件的文件
+rm -fr .git
+rm -rf .gitignore
+git init
+git checkout -b main
+git branch -d master
+git remote add -f origin $git_url
+git config core.sparsecheckout true # 开启 sparsecheckout 功能
+git sparse-checkout init
+echo "Factory" > .git/info/sparse-checkout
+git pull origin main
